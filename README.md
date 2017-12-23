fceee06fba105abea48f9c3a8265b960b9c9c7f4
Reed Weichler <rweichler@gmail.com>
2017-12-22 17:38:43 -0800

    initial commit + GNU bash

---
+++ README.md
@@ -0,0 +1,33 @@
+# Telesphoreo
+
+An attempt at reviving saurik's Telesphoreo project. Huge WIP.
+
+Right now this project is in the "find out what's even possible" stage. The only thing in this repo is GNU bash. The compile script [is currently a mess](https://github.com/rweichler/neotelesphoreo/blob/master/formula/bash/how2build.lua). The eventual goal is to make it look as nice and short as a [machomebrew forumla script](https://github.com/Homebrew/homebrew-core/blob/master/Formula/bash.rb).
+
+The choice of GNU bash mostly comes from its difficulty to port to other platforms. I figure once we figure out bash, then everything else should fall into place pretty easily.
+
+# How to cross-compile GNU bash for iOS
+
+* Be on a Mac (sorry)
+* Install the Xcode toolchain
+* Install LuaJIT (Either run `brew install luajit`, or, if you don't have homebrew, download [the LuaJIT source](http://luajit.org/download.html) and install it using [these instructions](http://luajit.org/install.html))
+* Install [aite](https://github.com/rweichler/aite)
+* `cd formula/bash`
+* `aite update`
+* `aite`
+
+
+# Resources
+
+Here's some stuff to put this repo into context:
+
+* [saurik's initial motivations behind Telesphoreo](http://www.saurik.com/id/1)
+* [Interview with Mike McQuaid (lead maintainer of machomebrew)](https://manifest.fm/1)
+
+> **saurik:** my main concern about machomebrew, a "main concern" that also applies to fink/macports, is to maintain compatibility with the existing package set, both in the specifics of package dependencies and selection as well as the generalities of overall goal and focus
+>
+> **saurik:** fwiw, homebrew had (and mostly still "has") the incorrect (even "naive", as they failed to learn from their predecessors) goal of depending entirely on the underlying system whenever possible. but apple's underlying system has no interest in maintaining library backwards compatibility nor does it ship with good versions of any packages (apple ships what they do because "we need to ship SOMETHING" and "but we should avoid the good stuff for license reasons")
+>
+> **saurik:** on ios these concerns are even stronger, given that the underlying library set is something apple doesn't even think anyone has any legitimate reason to be using, so they have been pretty brutal with compatibility. and they also don't really ship anything. and they certainly barely ship any underlying utilities. so the premise of homebrew essentially falls apart: there's no stable underlying base on which to build some second overlay universe of packages
+>
+> **saurik:** so the goal was to build, using the good packages (as even a fifteen year old version of GNU coreutils is better than a one minute old version of the BSD equivalents), an underlying base that felt as powerful as a linux distribution, with the kind of full-blown package manager capable of supporting that environment. I have never for one minute been unhappy with my choice of dpkg
---
+++ formula/bash/how2build.lua
@@ -0,0 +1,270 @@
+-- this is what is called when you run `aite`.
+function default()
+    if not(jit.os == 'OSX') then
+        error('You need to be on a Mac. Sorry.')
+    end
+    download()
+    run_autotools()
+    compile()
+    link()
+    print(CYAN('IT WORKED!!!! YAY! Your binary is in ')..GREEN('aite_build/bash')..CYAN('.'))
+end
+
+-- Here be dragons. I wouldn't try to understand this code.
+-- If you *really* want to try to understand this, a high level
+-- description is:
+--      Download bash
+--      Compile it for Mac (so it generates everything we need)
+--      Cross compile it for iOS using our own build system
+-- TODO: cut all of this down to < 50 LOC
+
+local mobile = true
+
+local function builder()
+    local b = _G.builder('apple')
+    if mobile then
+        b.sdk = 'iphoneos'
+        b.archs = {
+            'armv7',
+            'arm64',
+        }
+        b.cflags = ' -Wno-implicit-function-declaration'
+    else
+        b.cflags = ''
+    end
+    b.compiler = 'clang'
+    b.build_dir = 'aite_build'
+    b.cflags = b._cflags..' -g -O2 -Wno-parentheses -Wno-format-security'
+    function b:ar(obj)
+        print(YELLOW('---> ')..GREEN(self.build_dir..'/'..self.output))
+        os.execute('ar cr '..self.build_dir..'/'..self.output..' '..table.concat(obj, ' '))
+        os.execute('ranlib '..self.build_dir..'/'..self.output)
+    end
+    return b
+end
+
+local prefix = '/usr/local'
+local libdir = prefix..'/lib'
+local localedir = prefix..'/share/locale'
+
+function intl()
+    local b = builder()
+    b.defines = {
+        LOCALEDIR = '"'..localedir..'"',
+        LOCALE_ALIAS_PATH = '"'..localedir..'"',
+        LIBDIR = '"/usr/local/libdata"',
+        IN_LIBINTL = true,
+        ENABLE_RELOCATABLE = 1,
+        IN_LIBRARY = true,
+        INSTALLDIR = libdir,
+        NO_XMALLOC = true,
+        set_relocation_prefix = 'libitnl_set_relocation_prefix',
+        relocate = 'libitnl_relocate',
+        DEPENDS_ON_LIBICONV = 1,
+        HAVE_CONFIG_H = true,
+        SHELL = true,
+    }
+    b.include_dirs = {
+        'bash-4.4/lib/intl',
+        'bash-4.4',
+    }
+    b.src = fs.scandir('bash-4.4/lib/intl/*.c')
+    table.removecontents(b.src, 'bash-4.4/lib/intl/os2compat.c')
+    b.output = 'libintl.a'
+    b:ar(b:compile())
+end
+
+function termcap()
+    local b = builder()
+    b.defines = {
+        HAVE_CONFIG_H = true,
+    }
+    b.include_dirs = {
+        'bash-4.4/lib/termcap',
+        'bash-4.4',
+    }
+    b.src = fs.scandir('bash-4.4/lib/termcap/*.c')
+    b.output = 'libtermcap.a'
+    b:ar(b:compile())
+end
+
+function readline()
+    local b = builder()
+    b.defines = {
+        MACOSX = true,
+        HAVE_CONFIG_H = true,
+        SHELL = true
+    }
+    b.include_dirs = {
+        'bash-4.4/lib/readline',
+        'bash-4.4/lib/termcap',
+        'bash-4.4',
+        'bash-4.4/lib',
+    }
+    b.src = fs.scandir('bash-4.4/lib/readline/*.c')
+    table.removecontents(b.src, {
+        'bash-4.4/lib/readline/emacs_keymap.c',
+        'bash-4.4/lib/readline/vi_keymap.c',
+    })
+    b.output = 'libreadline.a'
+    b:ar(b:compile())
+end
+
+function bash()
+    local b = builder()
+    b.defines = {
+        PROGRAM = '"bash"',
+        CONF_HOSTTYPE = '"x86_64"',
+        CONF_OSTYPE = '"darwin15.6.0"',
+        CONF_MACHTYPE = '"x86_64-apple-darwin15.6.0"',
+        CONF_VENDOR = '"apple"',
+        LOCALEDIR = '"/usr/local/share/locale"',
+        PACKAGE = '"bash"',
+        SHELL = true,
+        HAVE_CONFIG_H = true,
+        MACOSX = true,
+    }
+    b.include_dirs = {
+        'bash-4.4',
+        'bash-4.4/include',
+        'bash-4.4/lib',
+        'bash-4.4/lib/intl',
+    }
+    b.src = fs.scandir('bash-4.4/*.c')
+    table.removecontents(b.src, 'bash-4.4/mksyntax.c')
+    b.output = 'libbash.a'
+    b:ar(b:compile())
+end
+
+function builtins()
+    local b = builder()
+    b.defines = {
+        HAVE_CONFIG_H = true,
+        SHELL = true,
+    }
+    b.include_dirs = {
+        'bash-4.4/builtins',
+        'bash-4.4',
+        'bash-4.4/include',
+        'bash-4.4/lib',
+        'bash-4.4/lib/intl',
+    }
+    local defs = fs.scandir('bash-4.4/builtins/*.def')
+    for i,v in ipairs(defs) do
+        os.execute('cd bash-4.4/builtins && ./mkbuiltins -D . ../../'..v)
+    end
+    b.src = fs.scandir('bash-4.4/builtins/*.c')
+    table.removecontents(b.src, 'bash-4.4/builtins/inlib.c')
+    table.removecontents(b.src, 'bash-4.4/builtins/gen-helpfiles.c')
+    table.removecontents(b.src, 'bash-4.4/builtins/mkbuiltins.c')
+    b.output = 'libbuiltins.a'
+    b:ar(b:compile())
+end
+
+function glob()
+    local b = builder()
+    b.defines = {
+        HAVE_CONFIG_H = true,
+        SHELL = true,
+        MACOSX = true,
+    }
+    b.include_dirs = {
+        'bash-4.4/lib/glob',
+        'bash-4.4',
+        'bash-4.4/include',
+        'bash-4.4/lib',
+    }
+    b.src = fs.scandir('bash-4.4/lib/glob/*.c')
+    table.removecontents(b.src, 'bash-4.4/lib/glob/glob_loop.c')
+    table.removecontents(b.src, 'bash-4.4/lib/glob/sm_loop.c')
+    b.output = 'libglob.a'
+    b:ar(b:compile())
+end
+
+function sh()
+    local b = builder()
+    b.defines = {
+        HAVE_CONFIG_H = true,
+        SHELL = true,
+        MACOSX = true,
+    }
+    b.include_dirs = {
+        'bash-4.4/lib/sh',
+        'bash-4.4',
+        'bash-4.4/lib',
+        'bash-4.4/include',
+        'bash-4.4/lib/intl',
+    }
+    b.src = fs.scandir('bash-4.4/lib/sh/*.c')
+    b.output = 'libsh.a'
+    b:ar(b:compile())
+end
+
+function tilde()
+    local b = builder()
+    b.defines = {
+        HAVE_CONFIG_H = true,
+        SHELL = true,
+        MACOSX = true,
+    }
+    b.include_dirs = {
+        'bash-4.4/lib/tilde',
+        'bash-4.4',
+        'bash-4.4/include',
+        'bash-4.4/lib',
+    }
+    b.src = 'bash-4.4/lib/tilde/tilde.c'
+    b.output = 'libtilde.a'
+    b:ar(b:compile())
+end
+
+function download()
+    if fs.isdir('bash-4.4') then return end
+
+    fs.mkdir(builder().build_dir)
+    local out = builder().build_dir..'/bash-4.4.tar.gz'
+
+    print(CYAN('Downloading GNU bash'))
+    os.execute('curl -L -f http://mirrors.syringanetworks.net/gnu/bash/bash-4.4.tar.gz -o '..out)
+    print(CYAN('Extracting GNU bash'))
+    os.execute('tar xf '..out)
+end
+
+function run_autotools()
+    if fs.isfile('bash-4.4/bash') then return end
+
+    print(CYAN('running autotools *throws up in mouth*'))
+    os.execute('cd bash-4.4 && ./configure')
+    print(CYAN('running make *slightly gags*'))
+    os.execute('cd bash-4.4 && make')
+end
+
+function compile()
+    print(CYAN('compiling for iOS with aite *sighs in relief*'))
+    local libs = {
+        'intl',
+        'readline',
+        'termcap',
+        'bash',
+        'builtins',
+        'glob',
+        'sh',
+        'tilde',
+    }
+    for i,v in ipairs(libs) do
+        print(YELLOW(v))
+        _G[v]()
+    end
+end
+
+function link()
+    local b = builder()
+    print(CYAN('linking *slightly gags again*'))
+    os.pexecute('clang -isysroot "'..b.sdk_path..'" -arch armv7 -arch arm64 '..b.build_dir..'/*.a -liconv -o '..b.build_dir..'/bash')
+    os.pexecute('ldid -S '..b.build_dir..'/bash')
+end
+
+function clean()
+    os.pexecute('rm -rf '..builder().build_dir)
+    os.pexecute('rm -rf bash-4.4')
+end
